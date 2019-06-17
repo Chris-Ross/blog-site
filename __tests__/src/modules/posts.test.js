bba2cde1be9616a474e12128f672aa89025d1043
daniel-testaguzza <danieltestaguzza@gmail.com>
2019-06-17 19:33:48 -0400

    passing test

--- __tests__/src/modules/posts.test.js
+++ __tests__/src/modules/posts.test.js
@@ -1,1 +1,28 @@
+const Post = require("../../../src/server/modules/post");
 
+describe("Post", () => {
+  test("should have and retrieve all fields", () => {
+    const title = "test title";
+    const body = "test body";
+    const author = "test author";
+    const publishDate = "test publish date";
+    const category = "test category";
+    const tags = "test tags";
+
+    const underTest = new Post(
+      title,
+      body,
+      author,
+      publishDate,
+      category,
+      tags
+    );
+
+    expect(underTest.title).toBe(title);
+    expect(underTest.body).toBe(body);
+    expect(underTest.author).toBe(author);
+    expect(underTest.publishDate).toBe(publishDate);
+    expect(underTest.category).toBe(category);
+    expect(underTest.tags).toBe(tags);
+  });
+});
--- src/server/modules/post.js
+++ src/server/modules/post.js
@@ -1,36 +1,36 @@
 class Post {
-    constructor(title, body, author, publishDate, category, tag) {
-        this._title = title;
-        this._body = body;
-        this._author = author;
-        this._publishDate = publishDate;
-        this._category = category;
-        this._tag = tag;
-    }
+  constructor(title, body, author, publishDate, category, tag) {
+    this._title = title;
+    this._body = body;
+    this._author = author;
+    this._publishDate = publishDate;
+    this._category = category;
+    this._tag = tag;
+  }
 
-    get title() {
-        return this._title;
-    }
+  get title() {
+    return this._title;
+  }
 
-    get body() {
-        return this._body;
-    }
+  get body() {
+    return this._body;
+  }
 
-    get author() {
-        return this._author;
-    }
+  get author() {
+    return this._author;
+  }
 
-    get publishDate() {
-        return this._publishDate;
-    }
+  get publishDate() {
+    return this._publishDate;
+  }
 
-    get category() {
-        return this._category;
-    }
+  get category() {
+    return this._category;
+  }
 
-    get tags() {
-        return this._tag;
-    }
+  get tags() {
+    return this._tag;
+  }
 }
 
 module.exports = Post;
