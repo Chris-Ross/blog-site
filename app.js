c6be7c06f711dc83dc5986a0c6260410273c830d
Chris-Ross <chrisjrossjr@gmail.com>
2019-06-17 16:23:05 -0400

    added post class

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var usersRouter = require("./src/server/routes/users");
 var app = express();
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "./src/server/views"));
 app.set("view engine", "hbs");
 
 app.use(logger("dev"));
